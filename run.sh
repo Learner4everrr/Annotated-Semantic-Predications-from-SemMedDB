0c9ff7dd3b1ab19073fa38ef5265a73a9382d7ea
Learner4everrr <zhan8023@umn.edu>
2024-03-13 16:38:48 -0500

    update

---
+++ run.sh
@@ -0,0 +1,37 @@
+CUDA_VISIBLE_DEVICES="0" python task.py -model bert-base-uncased -epoch 10 -random 3
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model distilbert-base-uncased -epoch 10 -random 3
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model roberta-base -epoch 10 -random 3
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model google-bert/bert-large-uncased -epoch 10 -random 3
+wait
+
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model bert-base-uncased -epoch 10 -random 94
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model distilbert-base-uncased -epoch 10 -random 94
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model roberta-base -epoch 10 -random 94
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model google-bert/bert-large-uncased -epoch 10 -random 94
+wait
+
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model bert-base-uncased -epoch 10 -random 20
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model distilbert-base-uncased -epoch 10 -random 20
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model roberta-base -epoch 10 -random 20
+wait
+
+CUDA_VISIBLE_DEVICES="0" python task.py -model google-bert/bert-large-uncased -epoch 10 -random 20
+wait
